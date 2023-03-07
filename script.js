3f94332f3106d8735bd755bc0dce8d3882486fb3
Louise Benson <louise.benson@gmail.com>
2023-03-07 11:42:41 +0000

    testing js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 let rates = document.querySelectorAll('.rating');
 let givenRating = sessionStorage.getItem("userRating");
 let ratesClicked = false;
-
+let submit = document.getElementById('submit')
 
 rates.forEach((rate) => {
     rate.addEventListener("click", () => {
       ratesClicked = true;
         sessionStorage.setItem("userRating", rate.innerHTML) ;
-        setRating()
-        rate.classList.add('selected')
+
+        
 })
   })
 
-  function setRating() {
+function setRating() {
     document.getElementById("rated").innerHTML = givenRating;
 
   }
 
-
+submit.addEventListener('click', setRating)
