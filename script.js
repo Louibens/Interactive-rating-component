2799b1fbdab3a06fb91be0914a03e6b3c6a4af3d
Louise Benson <louise.benson@gmail.com>
2023-03-09 12:32:26 +0000

    work on deselecting rating when user changes to different rating. Only working for previous or nnext siblings so need to rectify

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 let rates = document.querySelectorAll('.rating');
 
-rates.forEach((rate) => {
-    rate.addEventListener("click", () => {
-      document.getElementById("rated").innerHTML = rate.innerHTML;
-      rate.classList.add('selected')
-      
+let i;
+// rates.forEach((rate) => {
+//     rate.addEventListener("click", () => {
+//       document.getElementById("rated").innerHTML = rate.innerHTML;
+//       rate.classList.add('selected')
+//       if (previousElementSibling.classList === 'selected') {
+//         rate.classList.remove('selected')
+//       }
+//     }
+//     )}
+// )
+
+
+for (i = 0; i < rates.length; i++) {
+  rates[i].addEventListener("click", function() {
+    document.getElementById("rated").innerHTML = this.innerHTML;
+    this.classList.add("selected");
+    if (this.previousElementSibling.classList !== 'selected' || this.nextElementSibling.classList !== 'selected' ) {
+      this.previousElementSibling.classList.remove('selected')
+      this.nextElementSibling.classList.remove('selected')
+  } else {
+  this.classList.add("selected");
     }
-    )}
-)
+  });
+}
+
+
 
 function submitRate() {
 document.getElementById('first-side').classList.add('hide');
