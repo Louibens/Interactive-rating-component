c34cc18d2ded52ddcc656aa77edd8119ad925c98
Louise Benson <louise.benson@gmail.com>
2023-03-10 14:46:39 +0000

    fixed js so only one rate can be selected

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
 let rates = document.querySelectorAll('.rating');
+let selectedRate = null;
 
-let i;
-// rates.forEach((rate) => {
-//     rate.addEventListener("click", () => {
-//       document.getElementById("rated").innerHTML = rate.innerHTML;
-//       rate.classList.add('selected')
-//       if (previousElementSibling.classList === 'selected') {
-//         rate.classList.remove('selected')
-//       }
-//     }
-//     )}
-// )
-
-
-for (i = 0; i < rates.length; i++) {
+for (let i = 0; i < rates.length; i++) {
   rates[i].addEventListener("click", function() {
+    // Set the text of the rated element to the selected rating
     document.getElementById("rated").innerHTML = this.innerHTML;
+    
+    // Add the "selected" class to the clicked element
     this.classList.add("selected");
-    if (this.previousElementSibling.classList !== 'selected' || this.nextElementSibling.classList !== 'selected' ) {
-      this.previousElementSibling.classList.remove('selected')
-      this.nextElementSibling.classList.remove('selected')
-  } else {
-  this.classList.add("selected");
+    
+    // Remove the "selected" class from the previously selected element
+    if (selectedRate !== null) {
+      selectedRate.classList.remove('selected');
     }
+    
+    // Update the selected rate variable to the newly selected element
+    selectedRate = this;
   });
 }
-
-
 
 function submitRate() {
 document.getElementById('first-side').classList.add('hide');
